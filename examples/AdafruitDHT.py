7e998d279c5ab2df0ca0560d1dc92336d6f4dac8
qw26822548 <com26822548@example.com>
2018-11-29 14:28:50 +0800

    11/29

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -20,26 +20,46 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import sys
-
 import Adafruit_DHT
+import time
+import httplib, urllib
+import json
 
+deviceId = "D2LZf0U8"
+deviceKey = "JJ35LmQxdEnVnwIX" 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException,socket.error) as ex: 
+			print "Error: %s"
+			time.sleep(10)
+			# sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
 
 # Parse command line parameters.
-while True:
- sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22, 
                 '2302': Adafruit_DHT.AM2302 }
- if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
- else:
-    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-    sys.exit(1)
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+   sensor = sensor_args[sys.argv[1]]
+   pin = sys.argv[2]
+else:
+   print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+   print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+   sys.exit(1)
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
- humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -48,8 +68,15 @@ while True:
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
+while True:
+ h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
  if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+      payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+      {"dataChnId":"Temperature","values":{"value":t0}}]} 
+      post_to_mcs(payload)
+      time.sleep(10)
  else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+  print('Failed to get reading. Try again!')
+  sys.exit(1)
+ time.sleep(1)
